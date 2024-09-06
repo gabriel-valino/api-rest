0a4c69b9ab91962fda241e6a1ce371f6797e0dc8
gabriel <gabrielvalino25@hotmail.com>
2024-09-06 17:05:13 -0300

    feat: add error handling for API requests and exceptions

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,20 +1,25 @@
 import { Request, Response } from "express";
+import { AppError } from "../utils/AppError";
 
 export class ProductsController {
-
-
   index(req: Request, res: Response) {
     const { page, limit } = req.query
 
     if(!page && !limit){
       return res.send("Products")
     }
-  
+
     res.send(`Page ${page} of ${limit}`)
   }
 
   create(req: Request, res: Response) {
     const { name, price} = req.body
+
+    if(!name) {
+      throw new AppError("Nome do produto é obrigatório!")
+    }
+
+    // throw new AppError("Erro ao tentar criar um produto.")
 
     res.status(201).json({name, price, user_id: req.user_id})
   }
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
-import express from "express"
+import express, { Request, Response, NextFunction } from "express"
 import { routes } from "./routes"
+import { AppError } from "./utils/AppError"
 
 const PORT = 3333 
 
 const app = express()
 
 app.use(express.json())
+
 app.use(routes)
+
+app.use((error: any, req: Request, res: Response, _:NextFunction) => {
+
+  if(error instanceof AppError) {
+    return res.status(error.statusCode).json({message: error.message})
+  }
+
+  res.status(500).json({message: error.message})
+})
 
 app.listen(PORT, () => console.log('Server is running on port ' + PORT))
